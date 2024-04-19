913cefaba74ec4c3c694b8bc1e8e99da82a0ae4e
PierluigiPapa <pierluigipapa9@gmail.com>
2024-04-19 12:07:41 +0200

    commit_prova_pier_1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 
 @section('title', 'Home')
-    
+
 @section('content')
     <h1 class="text-danger">Ciao Mondo</h1>
+    <h2 class="text-center text-primary">Sono Pierluigi</h2>
 @endsection
